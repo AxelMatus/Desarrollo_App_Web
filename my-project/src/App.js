274c52f3c4796ea22cf5baa0970211c6444a7d92
AxelMatus <axelantonio@gmail.com>
2022-04-24 18:23:59 -0600

    Nuevo proyecto creado con tailwind

---
+++ my-project/src/App.js
@@ -0,0 +1,20 @@
+//import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className='padding-10vh margin-10vh border-2 border-gray-400'>
+      <div className="my_container">
+        <h1 className="text-3xl font-bold "> Desarrollo de Aplicaciones Web </h1>
+      </div>
+
+      <div className="my_container">
+        <h2 className='text-2xl font-bold'> Tarea 01 </h2>
+      </div>
+
+      <div className="padding-10vh my_container">
+        <p className='text-xl'> Axel Matus    B74597 </p>
+      </div>
+    </div>
+  )
+}
